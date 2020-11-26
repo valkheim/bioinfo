4548a9edbc02bcb3fe21f589fba030fd92588361
Charles PAULET <valkheim@protonmail.com>
2020-11-26 13:09:43 +0100

    refactor: s/_test_/_case/g

--- tests/test_sequence.py
+++ tests/test_sequence.py
@@ -2,35 +2,35 @@ import bioinfo.Sequence as Sequence
 
 
 def test_dna_to_rna():
-    def _test(dna_strand, expected):
+    def _case(dna_strand, expected):
         sequence = Sequence.Sequence(dna_strand)
         assert sequence.to_rna() == expected
         assert sequence.rna_strand == expected
 
-    _test('CAGCTGACTTTACTTCAGTA', 'CAGCUGACUUUACUUCAGUA')
-    _test('CCTCATAAGC', 'CCUCAUAAGC')
+    _case('CAGCTGACTTTACTTCAGTA', 'CAGCUGACUUUACUUCAGUA')
+    _case('CCTCATAAGC', 'CCUCAUAAGC')
 
 
 def test_complement():
-    def _test(dna_strand, expected):
+    def _case(dna_strand, expected):
         sequence = Sequence.Sequence(dna_strand)
         assert sequence.complement().strand == expected
 
-    _test('CAATCGTTACGTTA', 'GTTAGCAATGCAAT')
+    _case('CAATCGTTACGTTA', 'GTTAGCAATGCAAT')
 
 
 def test_inverse():
-    def _test(dna_strand, expected):
+    def _case(dna_strand, expected):
         sequence = Sequence.Sequence(dna_strand)
         assert sequence.invert().strand == expected
 
-    _test('', '')
-    _test('ACGT', 'TGCA')
+    _case('', '')
+    _case('ACGT', 'TGCA')
 
 
 def test_inverse_complement():
-    def _test(dna_strand, expected):
+    def _case(dna_strand, expected):
         sequence = Sequence.Sequence(dna_strand)
         assert sequence.complement().invert().strand == expected
 
-    _test('TTGCATACGTAAGCA', 'TGCTTACGTATGCAA')
+    _case('TTGCATACGTAAGCA', 'TGCTTACGTATGCAA')
